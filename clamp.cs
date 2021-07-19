a4d418342b1698c15ff7abe90a5fb2086203f35b
shahoreer <81192173+shahoreer@users.noreply.github.com>
2021-07-19 16:09:29 -0400

    Update clamp.cs

--- clamp.cs
+++ clamp.cs
@@ -4,17 +4,14 @@
 using System;
 class HelloWorld {
   static void Main() {
-    Console.WriteLine(Clamp(5,1,5));
+    Console.WriteLine(Clamp(8,1,5));
   }
   
   static int Clamp(int value,int min,int max){
-      if(value >= min && value <= max){
+      if(value >= min && value <= max)
           return value;
-      }else if(value < min){
+      if(value < min)
           return min;
-      }else{
           return max;
-      }
   }
-  
 }
